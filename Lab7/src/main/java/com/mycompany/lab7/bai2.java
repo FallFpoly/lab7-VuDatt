12b7b456e02fdddbe9e6365418a2a7079a8fc03c
vudat <vudat@vudat>
2025-10-16 10:17:28 +0700

    nop lap 7

--- Lab7/src/main/java/com/mycompany/lab7/bai2.java
+++ Lab7/src/main/java/com/mycompany/lab7/bai2.java
@@ -1,16 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.lab7;
 
-/**
- *
- * @author ICT
- */
-public class bai2 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+// Lớp trừu tượng SinhVienPoly
+abstract class SinhVienPoly {
+    String hoTen;
+    String nganh;
+
+    
+    public SinhVienPoly(String hoTen, String nganh) {
+        this.hoTen = hoTen;
+        this.nganh = nganh;
+    }
+
+    // Phương thức trừu tượng
+    public abstract double getDiem();
+
+    
+    public String getHocLuc() {
+        double diem = getDiem();
+        if (diem < 5) {
+            return "Yeu";
+        } else if (diem < 6.5) {
+            return "Trung Binh";
+        } else if (diem < 7.5) {
+            return "Kha";
+        } else if (diem < 9) {
+            return "Gioi";
+        } else {
+            return "Xuat Xac";
+        }
+    }
+   
+    
+    public void xuat() {
+        System.out.println("Ho Ten: " + hoTen);
+        System.out.println("Nghanh: " + nganh);
+        System.out.println("Diem: " + getDiem());
+        System.out.println("Hoc Luc: " + getHocLuc());
     }
 }
+
